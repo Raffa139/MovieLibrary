72b3aaacf4a860c09f025d3e682dbcebfb48a6e7
Raffael E <raffa_e@web.de>
2025-03-13 15:40:08 +0100

    Instantiate repository based on cli argument

--- src/movies.py
+++ src/movies.py
@@ -1,12 +1,30 @@
+import sys
 from repository.json_repository import JsonRepository
+from repository.csv_repository import CsvRepository
 from omdb.omdb_client import OmdbClient
 from app.movie_app import MovieApp
 from environment import get_api_key
 
 
+def instantiate_repository():
+    if len(sys.argv) < 2:
+        return JsonRepository("movies.json")
+
+    _, repo_file = sys.argv
+    _, repo_file_type = repo_file.split(".")
+
+    if repo_file_type.lower() == "json":
+        return JsonRepository(repo_file)
+    elif repo_file_type.lower() == "csv":
+        return CsvRepository(repo_file)
+    else:
+        print(f"Repository file of type {repo_file_type} not supported.")
+        sys.exit()
+
+
 def main():
     """Initializes the movie repository and runs the main menu."""
-    repo = JsonRepository("movies.json")
+    repo = instantiate_repository()
     omdb_client = OmdbClient(api_key=get_api_key())
     app = MovieApp(repo, omdb_client)
     app.start()
