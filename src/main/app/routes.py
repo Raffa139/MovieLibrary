cf7d978793fd7d8a9f2bd73d8f9915ceb2c4633b
Raffael E <raffa_e@web.de>
2025-03-25 20:05:22 +0100

    Implement index & user movies route, create and use broad html layout in templates

--- src/main/app/routes.py
+++ src/main/app/routes.py
@@ -6,5 +6,30 @@ bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def index():
-    movies = repo.find_all_movies()
-    return render_template("index.html", movies=movies)
+    users = repo.find_all_users()
+    return render_template("index.html", users=users)
+
+
+@bp.route("/users/<int:user_id>")
+def user_movies(user_id):
+    user = repo.find_user_by_id(user_id)
+
+    if not user:
+        return "Not Found", 404
+
+    return render_template("user_movies.html", username=user.username, user_movies=user.movies)
+
+
+@bp.route("/users/<int:user_id>", methods=["POST"])
+def add_user_movie(user_id):
+    pass
+
+
+@bp.route("/users/<int:user_id>/<int:movie_id>", methods=["UPDATE"])
+def update_user_movie(user_id, movie_id):
+    pass
+
+
+@bp.route("/users/<int:user_id>/<int:movie_id>", methods=["DELETE"])
+def delete_user_movie(user_id, movie_id):
+    pass
