d0107c5b46291bf3fbe7e4d84df10ce44a863927
Raffael E <raffa_e@web.de>
2025-03-26 15:15:20 +0100

    Enable deletion of user movies, show success and error messages

--- src/main/app/routes.py
+++ src/main/app/routes.py
@@ -6,20 +6,23 @@ bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def index():
-    message = request.args.get("message")
+    msg = request.args.get("msg")
+    msg_lvl = request.args.get("msg_lvl")
     users = repo.find_all_users()
-    return render_template("index.html", users=users, message=message)
+    return render_template("index.html", users=users, msg=msg, msg_lvl=msg_lvl)
 
 
 @bp.route("/users/<int:user_id>")
 def user_movies(user_id):
-    message = request.args.get("message")
+    msg = request.args.get("msg")
+    msg_lvl = request.args.get("msg_lvl")
     user = repo.find_user_by_id(user_id)
 
     if not user:
         return "Not Found", 404
 
-    return render_template("user_movies.html", user=user, user_movies=user.movies, message=message)
+    return render_template("user_movies.html", user=user, user_movies=user.movies, msg=msg,
+                           msg_lvl=msg_lvl)
 
 
 @bp.route("/users/<int:user_id>", methods=["POST"])
@@ -30,10 +33,18 @@ def add_user_movie(user_id):
 @bp.route("/users/<int:user_id>/update-movie/<int:movie_id>")
 def update_user_movie(user_id, movie_id):
     return redirect(
-        url_for("main.user_movies", user_id=user_id, message="Movie updated successfully!"))
+        url_for("main.user_movies", user_id=user_id, msg="Movie updated successfully!",
+                msg_lvl="success"))
 
 
 @bp.route("/users/<int:user_id>/delete-movie/<int:movie_id>")
 def delete_user_movie(user_id, movie_id):
-    return redirect(
-        url_for("main.user_movies", user_id=user_id, message="Movie deleted successfully!"))
+    success = repo.delete_user_movie(user_id, movie_id)
+
+    msg = "Movie deleted successfully!"
+    msg_lvl = "success"
+    if not success:
+        msg = "Failed to deleted movie!"
+        msg_lvl = "error"
+
+    return redirect(url_for("main.user_movies", user_id=user_id, msg=msg, msg_lvl=msg_lvl))
