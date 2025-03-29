9dab30723ae8dcde6b705feba4110c36b6eee8ac
Raffael E <raffa_e@web.de>
2025-03-29 12:23:47 +0100

    Add select input to change view mode of movie items and make them thinner or wider

---
+++ static/scripts/movie_view.js
@@ -0,0 +1,20 @@
+const handleMovieViewMode = () => {
+    const viewModeInput = document.querySelector("#movie-view");
+
+    if (viewModeInput) {
+        const changeClass = (movie, newClass) => {
+            ["compact", "normal", "wide"].forEach(e => movie.classList.remove(e));
+            movie.classList.add(newClass);
+        };
+
+        const handleChange = (event) => {
+            const viewMode = event.target.value;
+            const movies = document.querySelectorAll(".movie");
+            movies.forEach(movie => changeClass(movie, viewMode));
+        };
+
+        viewModeInput.addEventListener("change", handleChange);
+    }
+};
+
+handleMovieViewMode();
